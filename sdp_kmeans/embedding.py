ce053a6ffd664030374267d42e29b8ad06da42a4
marianotepper <Lulita75>
2017-04-18 16:59:04 -0400

    Code cleanup

--- sdp_kmeans/embedding.py
+++ sdp_kmeans/embedding.py
@@ -17,13 +17,17 @@ def sdp_kmeans_embedding(X, n_clusters, target_dim, ret_sdp=False):
 def spectral_embedding(mat, target_dim, discard_first=True):
     if discard_first:
         last = -1
+        first = target_dim - last
     else:
+        first = target_dim
         last = None
     if mat.shape[0] != mat.shape[1]:
         mat = mat.dot(mat.T)
     eigvals, eigvecs = eigh(mat)
-    eigvecs = eigvecs[:, -(target_dim + 1):last]
-    eigvals_crop = eigvals[-(target_dim+1):last]
+
+    sl = slice(-first, last)
+    eigvecs = eigvecs[:, sl]
+    eigvals_crop = eigvals[sl]
     Y = eigvecs.dot(np.diag(np.sqrt(eigvals_crop)))
     Y = Y[:, ::-1]
 
