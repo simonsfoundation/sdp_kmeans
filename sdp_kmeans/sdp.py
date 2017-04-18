f2cf1e5a459b774dfcaf9f1ef2e419707cc0ba39
marianotepper <Lulita75>
2017-04-18 17:02:21 -0400

    Symmetric NMF without building the Gram matrix

--- sdp_kmeans/sdp.py
+++ sdp_kmeans/sdp.py
@@ -3,7 +3,7 @@ import cvxpy as cp
 from functools import partial
 import numpy as np
 from scipy.optimize import minimize
-from sdp_kmeans.nmf import symnmf_admm
+from sdp_kmeans.nmf import symnmf_gram_admm
 from sdp_kmeans.utils import dot_matrix
 
 
@@ -85,15 +85,14 @@ def sdp_km_burer_monteiro(X, n_clusters, rank=None, maxiter=1e3, tol=1e-5):
                       -sigma1 * (np.trace(Y.T.dot(Y)) - n_clusters)) * Y
 
         delta -= ones.dot(lambda2.T.dot(Y)) + lambda2.dot(ones.T.dot(Y))
-        # YYt = Y.dot(Y.T)
-        # eet = ones.dot(ones.T)
+
         Yt1 = Y.T.dot(ones)
         delta += sigma2 * (Y.dot(Yt1).dot(Yt1.T) + ones.dot(Yt1.T).dot(YtY)
                            -2 * ones.dot(Yt1.T))
 
         return delta.flatten()
 
-    Y = symnmf_admm(X_norm.dot(X_norm.T), rank)
+    Y = symnmf_gram_admm(X_norm, rank)
 
     lambda1 = 0.
     lambda2 = np.zeros((len(X), 1))
@@ -118,16 +117,6 @@ def sdp_km_burer_monteiro(X, n_clusters, rank=None, maxiter=1e3, tol=1e-5):
         lambda2 -= step * sigma2 * (Y.dot(Y.T.dot(ones)) - ones)
 
         error.append(np.linalg.norm(Y - Y_old) / np.linalg.norm(Y_old))
-
-        # YtX = Y.T.dot(X_norm)
-        # print(n_iter, np.trace(YtX.dot(YtX.T)), error[-1],
-        #       'const', np.min(Y.dot(Y.T)), np.trace(Y.T.dot(Y)),
-        #       np.min(Y.dot(Y.T.dot(ones))),
-        #       np.max(Y.dot(Y.T.dot(ones))),
-        #       'cols',
-        #       np.min(ones.T.dot(Y).dot(Y.T)),
-        #       np.max(ones.T.dot(Y).dot(Y.T))
-        #       )
 
         if error[-1] < tol:
             break
