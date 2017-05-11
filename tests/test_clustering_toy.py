0f9f74053a459d772c972ebd6ab94421c22b47bf
marianotepper <Lulita75>
2017-05-11 06:04:42 -0400

    Tweak plots, change the use of connected components by a rescaled SDP matrix

--- tests/test_clustering_toy.py
+++ tests/test_clustering_toy.py
@@ -1,10 +1,9 @@
 from __future__ import absolute_import, print_function
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
-import numpy as np
 import os
 import seaborn.apionly as sns
-from sdp_kmeans import connected_components, sdp_kmeans
+from sdp_kmeans import log_scale, sdp_kmeans
 from data import toy
 from tests.utils import plot_matrix, plot_data_clustered
 
@@ -18,34 +17,24 @@ if not os.path.exists(dir_name):
 
 def test_multilayer(X, gt, n_clusters, filename):
     D, Q = sdp_kmeans(X, [n_clusters])
-    Q_labels, _ = connected_components(Q)
+    Q_log = log_scale(Q)
 
     sns.set_style('white')
     plt.figure(figsize=(12, 6), tight_layout=True)
-    gs = gridspec.GridSpec(1, 2, width_ratios=[1, 3])
 
-    gs_in = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs[0],
-                                             wspace=0, hspace=0,
-                                             height_ratios=(0.5, 1, 0.5))
-    ax = plt.subplot(gs_in[1, :])
+    ax = plt.subplot(141)
     plot_data_clustered(X, gt, ax=ax)
+    ax.set_title('Input dataset', fontsize='xx-large')
 
-    gs_in = gridspec.GridSpecFromSubplotSpec(2, 3, subplot_spec=gs[1],
-                                             wspace=.05, hspace=0)
-
-    titles = ['Input Gramian',
+    titles = ['Input Gramian $\mathbf{{D}}$',
               '$\mathbf{{Q}}$ ($K={0}$)'.format(n_clusters),
-              'Connected components']
-    for i, (M, t) in enumerate(zip([D, Q, Q_labels], titles)):
-        reps = M.dot(X)
-
-        ax = plt.subplot(gs_in[0, i])
+              '$\mathbf{{Q}}$ (non-linearly rescaled)']
+    for i, (M, t) in enumerate(zip([D, Q, Q_log], titles)):
+        ax = plt.subplot(1, 4, i + 2)
         plot_matrix(M, ax=ax)
         ax.set_title(t, fontsize='xx-large')
 
-        ax = plt.subplot(gs_in[1, i])
-        plot_data_clustered(reps, gt, marker='x', ax=ax)
-
+    plt.tight_layout()
     plt.savefig('{}{}.pdf'.format(dir_name, filename))
 
 
