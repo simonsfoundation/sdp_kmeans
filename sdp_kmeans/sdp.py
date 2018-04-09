Conditional gradient diag implementation and small fix in BM for short and fat input matrices

sdp_kmeans/sdp.py
@@ -51,7 +51,10 @@ def sdp_km_burer_monteiro(X, n_clusters, rank=None, maxiter=1e3, tol=1e-5):
         rank = 8 * n_clusters
 
     X_norm = X - np.mean(X, axis=0)
-    cov = X_norm.T.dot(X_norm)
+    if X_norm.shape[0] > X_norm.shape[1]:
+        cov = X_norm.T.dot(X_norm)
+    else:
+        cov = X_norm.dot(X_norm.T)
     X_norm /= np.trace(cov.dot(cov)) ** 0.25
 
     Y_shape = (len(X), rank)
@@ -223,6 +226,120 @@ def sdp_km_conditional_gradient(D, n_clusters, max_iter=2e3,
         row_sum = np.mean(Q.sum(axis=1))
         print('sum constraint', row_sum.min(), row_sum.max())
         print('trace constraint', np.trace(Q))
+        print('nonnegative constraint', np.min(Q), np.mean(np.minimum(Q, 0)))
+
+    print('final objective', np.trace(D.dot(Q)))
+
+    if track_stats:
+        return Q, rmse_list, obj_value_list
+    else:
+        return Q
+
+
+def sdp_km_cg_diag(D, n_clusters, max_iter=2e3,
+                   stop_tol_max=1e-1, stop_tol_rmse=1e-4,
+                   verbose=False, track_stats=False):
+    n = len(D)
+    one_over_n = 1. / n
+
+    def lagrangian(Q, lagrange_lower_bound, t):
+        obj = -np.sum(D * Q)
+        obj += np.sum(lagrange_lower_bound * (Q + one_over_n))
+        penalty = (t + 1) ** 0.5
+        obj += penalty * np.sum(np.minimum(Q + one_over_n, 0) ** 2)
+        return obj
+
+    def gradient(Q, lagrange_lower_bound, t):
+        delta = -D
+
+        delta += lagrange_lower_bound
+        penalty = (t + 1) ** 0.5
+        delta += penalty * np.minimum(Q + one_over_n, 0)
+        return delta
+
+    def solve_lp(grad, t):
+        # The following commented 2 lines are the mathematically
+        # friendly version of the 2 lines following them.
+        # ortho_mat = np.eye(n) - np.ones_like(D) / n
+        # A = ortho_mat.dot(grad).dot(ortho_mat)
+        grad11 = np.broadcast_to(grad.sum(axis=1) / n, (n, n))
+        A = grad - grad11 - grad11.T + grad.sum() / (n ** 2)
+
+        tol = (t + 1) ** -1
+        return sp.linalg.eigsh(A, k=1, which='SA', tol=tol)
+
+    def line_search(update, current, lagrange_lower_bound, t):
+        alpha_lower = 0
+        alpha_upper = 1
+
+        while np.abs(alpha_lower - alpha_upper) > 1e-4:
+            lin_comb_lower = alpha_lower * update + (1 - alpha_lower) * current
+            lin_comb_upper = alpha_upper * update + (1 - alpha_upper) * current
+
+            obj_lower = lagrangian(lin_comb_lower, lagrange_lower_bound, t)
+            obj_upper = lagrangian(lin_comb_upper, lagrange_lower_bound, t)
+
+            if obj_upper < obj_lower:
+                alpha_lower = 0.5 * (alpha_lower + alpha_upper)
+            else:
+                alpha_upper = 0.5 * (alpha_lower + alpha_upper)
+
+        return alpha_lower
+
+    if track_stats or verbose:
+        rmse_list = []
+        obj_value_list = []
+
+    Q = np.zeros_like(D)
+    lagrange_lower_bound = np.zeros(D.shape)
+    step = 1
+    n_inner_iter = 10
+
+    for t in range(int(max_iter)):
+        for inner_it in range(n_inner_iter):
+            grad = gradient(Q, lagrange_lower_bound, t)
+            s, v, = solve_lp(grad, t)
+
+            if s < 0:
+                # update = (n_clusters - 1) * np.outer(v, v)
+                update = np.outer(v, v)
+                update *= (n_clusters - 1) / (n * np.max(np.diag(update)))
+
+                eta_ls = line_search(update, Q, lagrange_lower_bound,
+                                     t * n_inner_iter + inner_it)
+                eta_fix = 2. / (t * n_inner_iter + inner_it + 2)
+                eta = np.maximum(eta_ls, eta_fix)
+                # eta = 2. / (t * n_inner_iter + inner_it + 2)
+                Q = (1 - eta) * Q + eta * update
+
+        Q_nneg = Q + one_over_n
+
+        rmse = np.sqrt(np.mean(Q_nneg[Q_nneg < 0] ** 2))
+        max_error = np.abs(np.min(Q_nneg[Q_nneg < 0])) / one_over_n
+
+        if track_stats or verbose:
+            obj_value_list.append(np.trace(D.dot(Q)))
+            rmse_list.append(rmse)
+
+        if max_error < stop_tol_max and rmse < stop_tol_rmse:
+            break
+
+        lagrange_lower_bound += step * Q_nneg
+        np.minimum(lagrange_lower_bound, 0, out=lagrange_lower_bound)
+
+        if verbose and t % 10 == 0:
+            row_sum = Q.sum(axis=1)
+            print('iteration', t, 'Q', -one_over_n, Q.min(), Q.max(), '|',
+                  row_sum.min(), row_sum.max(), '|',
+                  np.trace(Q), np.max(np.diag(Q)), np.trace(D.dot(Q)), '|',
+                  eta)
+
+    Q += one_over_n
+
+    if verbose:
+        row_sum = np.mean(Q.sum(axis=1))
+        print('sum constraint', row_sum.min(), row_sum.max())
+        print('diagonal constraint', np.max(np.diag(Q)))
         print('nonnegative constraint', np.min(Q), np.mean(np.minimum(Q, 0)))
 
     print('final objective', np.trace(D.dot(Q)))